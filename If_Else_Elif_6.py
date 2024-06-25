35a2f930467cecfb0455af5acb1ed2decf5576df
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-06-25 22:11:49 +0700

    python-tiga

--- If_Else_Elif_6.py
+++ If_Else_Elif_6.py
@@ -9,26 +9,24 @@ while True: # Loop ini akan terus berjalan sampai pernyataan break
         nilai = int(input("Masukan nilai ujian : ")) # Fungsi input String yang dikonversi ke Integer
     
         # Kondisional (tentukan hasil berdasarkan nilai)
-        if nilai >= 90: #  Jika menggunakan operator dengan rentang tertentu contoh rentang 90 hingga 100, dan tambahkan --> nilai >=10 and nilai <=100
+        if nilai >= 90 and nilai <= 100: #  Jika menggunakan operator dengan rentang tertentu contoh rentang 90 hingga 100, dan tambahkan --> nilai >=90 and nilai <=100
             hasil = "Lulus dengan nilai A"
 
-        elif nilai >= 80: # nilai >= 80 and nilai < 90:
+        elif nilai >= 80 and nilai < 90:
             hasil = "Lulus dengan nilai B"
 
-        elif nilai >= 70: # nilai >= 70 and nilai < 80:
+        elif nilai >= 70 and nilai < 80:
             hasil = "Lulus dengan nilai C"
 
-        elif nilai >= 60: # nilai >= 60 and nilai < 70:
+        elif nilai >= 60 and nilai < 70:
             hasil = "Lulus dengan nilai D"
 
-        #elif nilai >=0 and nilai < 60:
-            #hasil = "Tidak Lulus dengan nilai E"
-        #else:
-            #print("Nilai diluar jangkauan! Masukan nilai antara 0 hingga 100!")
-        
-        else:
+        elif nilai >=0 and nilai < 60:
             hasil = "Tidak Lulus dengan nilai E"
-        
+        else:
+            print("Nilai diluar jangkauan! Masukan nilai antara 0 hingga 100!")
+            continue
+
         #break # Menghentikan looping jika input valid
 
         print(f"Hasil Nilai : {hasil}") # Dinamis, jika user memasukan nilai berupa angka (karena string di konversi menjadi integer)
