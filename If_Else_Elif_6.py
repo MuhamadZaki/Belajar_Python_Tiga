48ddf2ea9758bd2a8dca0037a0105b53cd50c472
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-06-27 01:59:11 +0700

    python-tiga

--- If_Else_Elif_6.py
+++ If_Else_Elif_6.py
@@ -25,16 +25,24 @@ while True: # Loop ini akan terus berjalan sampai pernyataan break
             hasil = "Tidak Lulus dengan nilai E"
         else:
             print("Nilai diluar jangkauan! Masukan nilai antara 0 hingga 100!")
-            continue
-
-        #break # Menghentikan looping jika input valid
 
         print(f"Hasil Nilai : {hasil}") # Dinamis, jika user memasukan nilai berupa angka (karena string di konversi menjadi integer)
+
+        lanjut = input("Ingin memasukan nilai lagi tidak tobrut? (y/n) :")
+
+        if lanjut.lower() != 'y':
+            break
         
     except ValueError: # Pesan kesalahan
         print("Input tidak benar! Masukan angka!")
 
-# Noted if-elif-else :
+# Note if-elif-else :
 # 1. if     --> Merupakan pernyataan kondisional yang memeriksa apakah suatu kondisi benar (True), jika kondisi benar blok code yang berada di bawah if akan dijalankan
 # 2. elif   --> Merupakan singkatan dari else if, pernyataan ini digunakan untuk memeriksa kondisi tambahan jika kondisi if di atasnya salah (False) dan kita dapat memiliki beberpa elif dalam satu rangkaian pernyataan kondisional
 # 3. else   --> Merupakan pernyataan yang digunakan untuk menangani semua kondisi lain yang tidak terpenuhi oleh if dan elif dan code di bawah else akan dijalankan ketika semua kondisi sebelumnya salah (False)
+
+# Note while dan for:
+# 1. while Looop --> Digunakan untuk menjalankan blok code selama kondisi yang ditentukan benar (True). Ini cocok untuk kasus di mana kita perlu terus menjalankan suatu proses berdasarkan input atau kondisi yang diuji
+# 2. for Loop   --> digunakan untuk mengulangi item dalam suatu urutan seperti (list, tuple, string, dict, set) atau objek yang dapat diiterasi lainnya
+
+"""  Note untuk kasus ini for --> Di python int tidak dapat diiterasi menggunakan Loop for dalam konteks ini. Kamu bisa menggunakan while! """
---
+++ Perulangan_For_7.py
@@ -0,0 +1,32 @@
+""" Perulangan for Termasuk Penjelasan Dasar, Iterasi Melalui Berbagai Tipe Data """
+
+# Perulangan for digunakan untuk mengulangi item-item dalam urutan seperti (list, tuple, dict, set dan string)
+
+# 1. Sintaks dasar perulangan for
+
+#for item in itrable:
+    #Lakukan sesuatu dengan item
+
+# 2. Itersi melalui list
+
+list_buah = ["Semangka", "Tobrut", "Jeruk", "Durian"]
+
+for buah in list_buah:
+    print(buah) # Hasilnya adalah Semangka, Tobrut, Jeruk, Durian
+
+# 3. Iterasi melalui string
+
+toket_brutal = "Ukhti"
+
+for huruf in toket_brutal:
+    print(huruf) # Hasilnya adalah ukhti
+
+# 4. Iterasi melalui tuple
+
+tuple_buah = ("Anggur", "Jeruk", "Nanas")
+
+for buah in  tuple_buah:
+    print(buah) # Hasilnya adalah Amggur, Jeruk, Nanas
+
+
+# 5. Iterasi melaui dictionary
