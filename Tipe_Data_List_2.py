f5e7ea03eea5bdb539a9bb502dfe64f2c31e2082
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-07-14 09:45:29 +0700

    python-tiga

--- Tipe_Data_List_2.py
+++ Tipe_Data_List_2.py
@@ -25,39 +25,39 @@ list_integer = [1,2,3,4,5,6,7,8,9,0] # --> Inisialisasi variabel yang menyimpan 
 
 
 # List yang Berisi Campuran Tipe Data (int, float, string, boolean)
-list_campuran = [100, 10.0, 'Tobrut', True] # --> Inisialisasi list dengan 4 elemen
+list_campuran = [100, 10.0, 'Tobrut', True] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data integer, float, string dan boolean
 
-print('List Kosong :', list_kosong)         # --> []
-print('List_String :', list_string)         # --> Manusia, adalah, mahluk, Bumi
-print('List Integer :', list_integer)       # --> 1,2,3,4,5,6,7,8,9,0
-print('list Campuran :', list_campuran)     # --> 100, 10.0, Tobrut, True
+print('List Kosong :', list_kosong)         # --> []                                --> (String di dalam cuma untuk pemanis)
+print('List_String :', list_string)         # --> Manusia, adalah, mahluk, Bumi     --> (String di dalam cuma untuk pemanis)
+print('List Integer :', list_integer)       # --> 1,2,3,4,5,6,7,8,9,0               --> (String di dalam cuma untuk pemanis)
+print('list Campuran :', list_campuran)     # --> 100, 10.0, Tobrut, True           --> (String di dalam cuma untuk pemanis)
 
 
 """ Mengambil Indeks Dari List (Indeks dimulai dari 0) """
+ 
+print('Indeks 0 dari List_String :',list_string[0])         # --> Mengembalikan elemen pertama dari list_string dan hasilnya adalah manusia   --> (String di dalam cuma untuk pemanis)
 
-print('Indeks 0 dari List_String :',list_string[0])         # --> Mengembalikan elemen pertama dari list_string dan hasilnya adalah manusia
+print('Indeks 1 dari List_Integer : ', list_integer[1])     # --> Mengembalikan elemen kedua  dari list_integer dan hasilnya adalah 2         --> (String di dalam cuma untuk pemanis)
 
-print('Indeks 1 dari List_Integer : ', list_integer[1])     # --> Mengembalikan elemen kedua  dari list_integer dan hasilnya adalah 2
-
-print('Indeks 4 dari List_Campuran :', list_campuran[3])    # --> Mengembalikan elemen ke empat dari list_campuran dan hasilnya adalah True
+print('Indeks 4 dari List_Campuran :', list_campuran[3])    # --> Mengembalikan elemen ke empat dari list_campuran dan hasilnya adalah True   --> (String di dalam cuma untuk pemanis)
 
 
 """ Mengambil Indeks negatif """
 
-print('Indeks -1 dari List_String :',list_string[-1])       # --> Mengembalikan elemen terakhir dari list_string dan hasilnya adalah Bumi
+print('Indeks -1 dari List_String :',list_string[-1])       # --> Mengembalikan elemen terakhir dari list_string dan hasilnya adalah Bumi               --> (String di dalam cuma untuk pemanis)
 
-print('Indeks -2 dari List_Integer : ', list_integer[-2])   # --> Mengembalikan elemen kedua dari terakhir dari list_integer dan hasilnya adalah 9
+print('Indeks -2 dari List_Integer : ', list_integer[-2])   # --> Mengembalikan elemen kedua dari terakhir dari list_integer dan hasilnya adalah 9      --> (String di dalam cuma untuk pemanis)
 
-print('Indeks -3 dari List_Integer : ', list_integer[-3])   # --> Mengembalikan elemen ketiga dari terakhir dari list_integer dan hasilnya adalah 8
+print('Indeks -3 dari List_Integer : ', list_integer[-3])   # --> Mengembalikan elemen ketiga dari terakhir dari list_integer dan hasilnya adalah 8     --> (String di dalam cuma untuk pemanis)
 
-print('Indeks -4 dari List_Campuran :', list_campuran[-4])  # --> Mengembalikan elemen keempat dari terakhir dari list_campuran dan hasilnya adalah 100
+print('Indeks -4 dari List_Campuran :', list_campuran[-4])  # --> Mengembalikan elemen keempat dari terakhir dari list_campuran dan hasilnya adalah 100 --> (String di dalam cuma untuk pemanis)
 
 print('-------------')
 
 """ Slicing List atau Teknik Memotong Value Pada List """
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian', 'Nanas'] # --> Inisialisasi list dengan 5 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian', 'Nanas'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)                # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 print(list_buah[0:1])   # --> Mengambil elemen dari indeks 0 hingga sebelum indeks 1 dan hasilnya adalah Anggur
 
@@ -77,8 +77,8 @@ print('-------------')
 
 """ Slicing Tanpa Batas """
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 print(list_buah[0:])  # --> Mengambil semua elemen dari indeks 0 sampai akhir dan hasilnya adalah Anggur, Melon, Jeruk, Durian
 
@@ -100,8 +100,8 @@ print('-------------')
 
 """ Mengubah Data di Dalam List (changable/bisa diubah) """
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah[0] = 'Nanas'                             # --> Mengubah elemen pertama (indeks 0)
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Nanas, Melon, Jeruk, Durian
@@ -116,99 +116,99 @@ print('-------------')
 
 """ Mengubah Data dalam Range Pada List """
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah[1:3] = ['Nangka', 'Semangka']            # --> Mengubah elemen pada indeks 1 sampai 2 (termasuk) 
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Anggur, Nangka, Semangka, Durian
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah[2:4] = ['Nangka', 'Semangka']            # --> Mengubah elemen pada indeks 2 sampai 3 (termasuk) 
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Anggur, Melon, Nangka, Semangka
 
 print('-------------')
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah.append('Sirsak')                         # --> Menambahkan elemen baru di indeks akhir 
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Anggur, Melon, Jeruk, Durian, Sirsak
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah.clear()                                  # --> Menghapus semua elemen pada list 
 print(list_buah)                                   # --> Mencetak list_buah dan hasilnya adalah kosong []
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buat Utama : ')
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang meyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buat Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah_copy = list_buah.copy()                  # --> Membuat salinan baru dari list 
-print('Salinan List_Buah : ', list_buah_copy)      # --> Mencetak variabel list_buah_copy dan hasilnya adalah Anggur, Melon, Jeruk, Durian
+print('Salinan List_Buah : ', list_buah_copy)      # --> Mencetak variabel list_buah_copy dan hasilnya adalah Anggur, Melon, Jeruk, Durian --> (String di dalam cuma untuk pemanis)
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah.insert(0, 'Sirsak')                      # --> Menambahkan elemen baru Sirsak di posisi indeks 0 
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Sirsak, Anggur, Melon, Jeruk, Durian
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah.insert(2, 'Sirsak')                      # --> Menambahkan elemen baru sirsak di posisi indeks 2 
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Anggur, Melon, Sirsak, Jeruk, Durian
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 index_jeruk = list_buah.index('Jeruk')             # --> Mengembalikan posisi indeks pertama dari elemen Jeruk 
-print('Indeks dari Jeruk : ', index_jeruk)         # --> Mencetak variabel index_jeruk dan hasilnya adalah 2
+print('Indeks dari Jeruk : ', index_jeruk)         # --> Mencetak variabel index_jeruk dan hasilnya adalah 2 --> (String di dalam cuma untuk pemanis)
 
 print('-------------')
 
-list_angka = [1, 2, 3, 4, 5]                       # --> Inisialisasi list dengan 5 elemen
-print('Hasil List_Angka utama :', list_angka)
+list_angka = [1, 2, 3, 4, 5]                       # --> Inisialisasi variabel yang meyimpan data list, berisi 5 elemen data integer
+print('Hasil List_Angka utama :', list_angka)      # --> Mencetak variabel list_angka --> (String di dalam cuma untuk pemanis)
 
 angka_yang_dihapus = list_angka.pop()              # --> Menghapus dan mengembalikan elemen akhir dari list 
-print('Angka yang di Hapus : ',angka_yang_dihapus) # --> Mencetak variabel dan hasilnya adalah 5
+print('Angka yang di Hapus : ',angka_yang_dihapus) # --> Mencetak variabel dan hasilnya adalah 5 --> (String di dalam cuma untuk pemanis)
 
-list_angka = [1, 2, 2, 3, 4, 5]                    # --> Inisialisasi list dengan 5 elemen
-print('Hasil List_Angka utama :', list_angka)
+list_angka = [1, 2, 2, 3, 4, 5]                    # --> Inisialisasi variabel yang menyimpan data list, berisi 5 elemen data integer
+print('Hasil List_Angka utama :', list_angka)      # --> Mencetak variabel list_angka --> (String di dalam cuma untuk pemanis)
 
 jumlah_dua = list_angka.count(2)                   # --> Menghitung berapa kali nilai 2 muncul di dalam list 
-print('Jumlah angka 2 : ', jumlah_dua)             # --> Mencetak variabel jumlah_angka dan hasilnya adalah 2
+print('Jumlah angka 2 : ', jumlah_dua)             # --> Mencetak variabel jumlah_angka dan hasilnya adalah 2 --> (String di dalam cuma untuk pemanis)
 
-list_angka = [1, 2, 2, 3, 4, 5]                    # --> Inisialisasi list dengan 5 elemen
-print('Hasil List_Angka utama :', list_angka)
+list_angka = [1, 2, 2, 3, 4, 5]                    # --> Inisialisasi variabel yang menyimpan data list, berisi 5 elemen data integer
+print('Hasil List_Angka utama :', list_angka)      # --> Mencetak variabel list_angka --> (String di dalam cuma untuk pemanis)
 
 list_angka.remove(1)                               # --> Menghapus elemen pertama yang memiliki nilai 1 dari list 
 print(list_angka)                                  # --> Mencetak variabel list_angka dan hasilnya adalah 2, 2, 3, 4, 5 
 
 print('-------------')
 
-list_buah = ['Anggur', 'Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 5 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 5 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)                 # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah.remove('Anggur')                                   # --> Menghapus elemen pertama yang memiliki nilai Anggur dari list 
 print(list_buah)                                             # --> Mencetak variabel list_buah dan hasilnya adalah Anggur, Melon, Jeruk, Durian
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian']           # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian']           # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)                 # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 del list_buah[-1]                                            # --> Menghapus elemen terakhir dari list 
 print(list_buah)                                             # --> Mencetak variabel list_buah dan hasilnya adalah Anggur, Melon, Jeruk
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 del list_buah[0]                                   # --> Menghapus elemen pertama dari list 
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Melon, Jeruk, Durian
 
 
-list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi list dengan 4 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Anggur', 'Melon', 'Jeruk', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 4 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)       # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 del list_buah[0:2]                                 # --> Menghapus elemen dari indeks 0 hingga 1 (indeks 0 dan 1) dari list 
 print(list_buah)                                   # --> Mencetak variabel list_buah dan hasilnya adalah Jeruk, Durian
@@ -216,31 +216,31 @@ print(list_buah)                                   # --> Mencetak variabel list_
 print('-------------')
 
 """ Menggabungkan Dua Buah List atau Lebih """
-a = [1, 2, 3]           # --> Inisialisasi list dengan 3 elemen
-b = ['a']               # --> Inisialisasi list dengan 1 elemen
-c = [True, 'b', False]  # --> Inisialisasi list dengan 3 elemen
+a = [1, 2, 3]           # --> Inisialisasi variabel yang menyimpan data list, berisi 3 elemen data integer
+b = ['a']               # --> Inisialisasi variabel yang menyimpan data list, berisi 1 elemen data string
+c = [True, 'b', False]  # --> Inisialisasi variabel yan gmenyimpan data list, berisi 3 elemen data boolean, string dan boolean
 
-list_baru = a + b + c   # --> Menggebungkan ketiga list menjadi list tunggal dengan urutan elemen yang sesuai dengan urutan penjumlahan
+list_baru = a + b + c   # --> Menggebungkan ketiga list menjadi list tunggal dengan urutan elemen yang sesuai dengan urutan penjumlahan (menggunakan operator penjumlahan) dan nanti tersimpan pada variabel list_baru
 print(list_baru)        # --> Mencetak variabel list_baru dan hasilnya adalah 1, 2, 3, a, True, b, False
 
 print('-------------')
 
-list_buah = ['Mangga', 'Jeruk', 'Zaitun', 'Apel', 'Durian'] # --> Inisialisasi list dengan 5 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Mangga', 'Jeruk', 'Zaitun', 'Apel', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 5 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)                # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)
 
 list_buah.sort() # --> Mengurutkan list secara asceding atau menaik, yaitu elemen yang diurutkan dari yang terkecil ke yang terbesar berdasarkan urutan alfabet untuk string 
 print(list_buah) # --> Mencetan variabel list_buah dan hasilnya adalah Apel, Durian, Jeruk, Mangga, Zaitun
 
 
-list_buah = ['Mangga', 'Jeruk', 'Zaitun', 'Apel', 'Durian'] # --> Inisialisasi list dengan 5 elemen
-print('Hasil List_Buah Utama : ', list_buah)
+list_buah = ['Mangga', 'Jeruk', 'Zaitun', 'Apel', 'Durian'] # --> Inisialisasi variabel yang menyimpan data list, berisi 5 elemen data string
+print('Hasil List_Buah Utama : ', list_buah)                # --> Mencetak variabel list_buah --> (String di dalam cuma untuk pemanis)        
 
 list_buah.reverse() # --> Membalik urutan elemen dalam list, yaitu elemen akan diputar ke arah yang berlawanan dari urutan sebelumnya 
 print(list_buah)    # --> Mencetak variable list_buah dan hasilnya adalah Durian, Apel, Zaitun, Jeruk, Mangga
 
 
-list_buah = ['Mangga', 'Jeruk', 'Zaitun'] # --> Inisialisasi list dengan 3 elemen
-buah_tambahan = ['Apel', 'Durian']        # --> Inisialisasi list dengan 2 elemen
+list_buah = ['Mangga', 'Jeruk', 'Zaitun'] # --> Inisialisasi variabel yang menyimpan data list, berisi 3 elemen data string
+buah_tambahan = ['Apel', 'Durian']        # --> Inisialisasi variabel yang menyimpan data list, berisi 2 elemen data string
 
 list_buah.extend(buah_tambahan)           # --> Menambahkan semua elemen dari list, yaitu di ikuti dari semua elemen dari variabel baru list 
 print(list_buah)                          # --> Mencetak variabel list_buah dan hasilnya adalah Mangga, Jeruk, Zaitun, Apel, Durian
--- Tipe_Data_Tuple_3.py
+++ Tipe_Data_Tuple_3.py
@@ -10,79 +10,79 @@
 """ Tipe Data Tuple (Ordered-Terurut) - (Unchnagble-Tidak Bisa diubah) """
 
 # Cara Standar Tuple
-standar_tuple = ('Laki-laki', 'Perempuan') # --> Inisialisasi tuple menyimpan 2 elemen
+standar_tuple = ('Laki-laki', 'Perempuan') # --> Inisialisasi variabel yang menyimpan data tuple, berisi 2 elemen data string
 print(standar_tuple)                       # --> Mencetak variabel standar_tuple dan hasilnya adalah Laki-laki, Perempuan
 
 # Tanpa Kurung Tuple
-tanpa_kurung_tuple = 'Tobrut', 'Ukhti'   # --> Inisialisasi tupel menyimpan 2 elemen
+tanpa_kurung_tuple = 'Tobrut', 'Ukhti'   # --> Inisialisasi variabel yang menyimpan data tuple, berisi 2 elemen data string
 print(tanpa_kurung_tuple)                # --> Mencetak variabel dan hasilnya adalah Tobrut, Ukhti
 
-Ini_sting_bukan_tuple = 'Tobrut'         # --> Inisialisasi variabel dengan nilai string (Ingat yang ini bukan tuple)
+Ini_sting_bukan_tuple = 'Tobrut'         # --> Inisialisasi variabel yang menyimpan data string (Ingat yang ini bukan tuple)
 print(Ini_sting_bukan_tuple)             # --> Mencetak variabel dan hasilnya adalah Tobrut
 
 # Tuple Kosong
-tuple_kosong = ()   # --> Inisialisasi tuple kosong
+tuple_kosong = ()   # --> Inisialisasi variabel yang menyimpan data tuple, tidak berisi elemen apapun atau kosong
 print(tuple_kosong) # --> Mencetak variabel tuple_kosong dan hasinya adalah  ()
 
 # Tupel Tunggal (Hanya Berisi Satu Item)
-tuple_tunggal = (10) #    --> Inisialisasi 
-print(tuple_tunggal) # 10 --> Akan tetapi di Anggap Integer
+tuple_tunggal = (10) #    --> Inisialisasi variabel yang menyimpan data integer (Akan tetapi di Anggap Integer)
+print(tuple_tunggal) # 10 --> Mencetak variabel tupel_tunggal
 
-tuple_tunggal = (10,) #    --> Inisialisasti
-print(tuple_tunggal)  # 10 --> Ini Baru dianggap Tuple
+tuple_tunggal = (10,) #    --> Inisialisasi variabel yang menyimpan data tuple, berisi 1 elemen data integer (Ini Baru dianggap Tuple)
+print(tuple_tunggal)  # 10 --> Mencetak variabel tuple_tunggal
 
 print('-------------')
 
 # Cara Standar (Mengakses Nilai Tuple dengan Langsung Mendefinisikan Indeks)
-cara_standar_tuple = ('Tobrut', 'Era')
-print(cara_standar_tuple[1]) # Era
-print(cara_standar_tuple[0]) # Tobrut
+cara_standar_tuple = ('Tobrut', 'Era') # --> Inisialisasi variabel yang menyimpan data tuple, berisi 2 elemen data string
+print(cara_standar_tuple[1])           # Era       --> Mencetak variabel
+print(cara_standar_tuple[0])           # Tobrut    --> Mencetak variabel
 
 # Mengakses Nilai Tuple dengan Negatif Indeks
-cara_standar_tuple = ('Tobrut', 'Era')
-print(cara_standar_tuple[-1]) # Era
-print(cara_standar_tuple[-2]) # Tobrut
+cara_standar_tuple = ('Tobrut', 'Era') # --> Inisialisasi variabel yang menyimpan data tuple, berisi 2 elemen data string
+print(cara_standar_tuple[-1])          # Era    --> Mencetak vriabel
+print(cara_standar_tuple[-2])          # Tobrut --> Mencetak variabel
 
 
 # Slicing Tuple
-tuple_buah = ('Anggur', 'Melon', 'Nanas', 'Semangka')
-print(tuple_buah[0:])    # Anggur, Melon, Nanas, Semangka dan Tak-terbatas
-print(tuple_buah[0:2])   # Anggur, Melon
-print(tuple_buah[1:3])   # Melon, Nanas
-print(tuple_buah[0:-1])  # Anggur, Melon, Nanas
-print(tuple_buah[-1:-3]) # Kosong
-print(tuple_buah[-1:3])  # Kosong
-print(tuple_buah[-3:-1]) # Melon, Nanas
+tuple_buah = ('Anggur', 'Melon', 'Nanas', 'Semangka') # --> Inisialisasi variabel yang menyimpan data tuple, berisi 4 elemen data string
+print(tuple_buah[0:])                                 # Anggur, Melon, Nanas, Semangka dan Tak-terbatas --> Mencetak variabel dan slicing
+print(tuple_buah[0:2])                                # Anggur, Melon                                   --> Mencetak variabel dan slicing
+print(tuple_buah[1:3])                                # Melon, Nanas                                    --> Mencetak variabel dan slicing
+print(tuple_buah[0:-1])                               # Anggur, Melon, Nanas                            --> Mencetak variabel dan slicing
+print(tuple_buah[-1:-3])                              # Kosong                                          --> Mencetak variabel dan slicing
+print(tuple_buah[-1:3])                               # Kosong                                          --> Mencetak variabel dan slicing
+print(tuple_buah[-3:-1])                              # Melon, Nanas                                    --> Mencetak variabel dan slicing
 
 # Slicing Tanpa Batas
-tuple_buah = ('Anggur', 'Melon', 'Nanas', 'Semangka')
-print(tuple_buah[0:])    # Anggur, Melon, Nanas, Semangka
-print(tuple_buah[1:])    # Melon, Nanas, Semangka
-print(tuple_buah[2:])    # Nanas, Semangka
-print(tuple_buah[3:])    # Semangka dan Tak-terbatas
-print(tuple_buah[:0])    # []
-print(tuple_buah[:1])    # Anggur
-print(tuple_buah[:2])    # Anggur, Melon
-print(tuple_buah[:3])    # Anggur, Melon, Nanas
-print(tuple_buah[:4])    # Anggur, Melon, Nanas, Semangka
+tuple_buah = ('Anggur', 'Melon', 'Nanas', 'Semangka') # --> Inisialisasi variabel yang menyimpan data tuple, berisi 4 elemen data string
+print(tuple_buah[0:])                                 # Anggur, Melon, Nanas, Semangka   --> Mencetak variabel dan slicing
+print(tuple_buah[1:])                                 # Melon, Nanas, Semangka           --> Mencetak variabel dan slicing
+print(tuple_buah[2:])                                 # Nanas, Semangka                  --> Mencetak variabel dan slicing
+print(tuple_buah[3:])                                 # Semangka dan Tak-terbatas        --> Mencetak variabel dan slicing
+print(tuple_buah[:0])                                 # []                               --> Mencetak variabel dan slicing
+print(tuple_buah[:1])                                 # Anggur                           --> Mencetak variabel dan slicing
+print(tuple_buah[:2])                                 # Anggur, Melon                    --> Mencetak variabel dan slicing
+print(tuple_buah[:3])                                 # Anggur, Melon, Nanas             --> Mencetak variabel dan slicing
+print(tuple_buah[:4])                                 # Anggur, Melon, Nanas, Semangka   --> Mencetak variabel dan slicing
 
 print('-------------')
 
 # Squence Unpaking (Mengekstrak Isi Dari Tuple ke Dalam Variable-Variable Tunggal, Secara Berurutan)
-siswi = (' Natasia Tobrut', 'Makassar', 22)
+siswi = (' Natasia Tobrut', 'Makassar', 22) # --> Inisialisasi variabel yang menyimpan data tuple, berisi 3 elemen data string, string dan integer
 
 nama, asal, usia = siswi # Ekstrak Data atau Juga dinamakan Sequence Unpacking (Setiap Variable Akan Memiliki Nilai dari Variable siswi)- yang pertama, nama maka mempunyai indeks [0]
-print('Nama :', nama)    # Natasia Tobrut --> siswi[0] -nama
-print('Asal : ', asal)   # Makassar       --> siswi[1] -asal
-print('Usia : ', usia)   # 22             --> siswi[2] -usia
+print('Nama :', nama)    # Natasia Tobrut --> siswi[0] -nama   --> Mencetak variabel (String di dalam cuma untuk pemanis)
+print('Asal : ', asal)   # Makassar       --> siswi[1] -asal   --> Mencetak variabel (String di dalam cuma untuk pemanis)
+print('Usia : ', usia)   # 22             --> siswi[2] -usia   --> Mencetak variabel (String di dalam cuma untuk pemanis)
 
 print('-------------')
 
 # Menggabungkan Dua Buat Tuple atau Lebih Menjadi Satu
-a = (1,2,3)
-b = (4,5,6)
-c = a + b
-print('Hasil = ', c)    # 1,2,3,4,5,6
+a = (1,2,3)          # --> Inisialisasi variabel yang menyimpan data tuple, berisi 3 elemen data integer
+b = (4,5,6)          # --> Inisialisasi variabel yang menyimpan data tuple, berisi 3 elemen data integer
+c = a + b            # --> Menggabungkan data tuple pada variabel a dan b menggunaan operator penjumlahan dan tersimpan dalam variabel c
+print('Hasil = ', c) # 1,2,3,4,5,6 --> Mencetak variabel c (String di dalam cuma untuk pemanis)
 
 # FUNGSI - FUNGSI BAWAAN TUPLE
 # 1. len() ---> Menghitung jumlah item pada tuple
