e289bcd62f0978d9a0fd98a026abdbd199193d52
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-07-22 21:41:29 +0700

    python-tiga

--- Basic/operators_5.py
+++ Basic/operators_5.py
@@ -167,4 +167,164 @@ print(results)    # --> Mencetak variabel
 
 print("-------->")      # -- Abaikan ini
 
-# ISTIRAHAT DUYU YAH
+a = False       # --> Inisialisasi variabel yang menyimpan daya boolean
+results = not a # --> Menggunakan operator not, membalikan nilai variabel a lalu jika nilai variabel False maka akan menghasilkan True dan tersipan pada variabel results
+print(results)  # --> Mencetak variabel
+
+
+# 4. Operator Penugasan --> Untuk menetapkan nilai ke variabel
+
+""" Penugasan (=, Menetapkan Nilai Ke Variabel) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data integer
+print(a) # --> Mencetak variabel
+
+""" Penugasan (+=, Menambahkan Nilai Ke Variabel Dan Menetapkan Hasilnya) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data integer
+a += 3   # --> Menambahkan nilai variabel a dan menetapkan hasilnya ke variabel itu sendiri
+print(a) # --> Mencetak variabel
+
+""" Penugasan (-=, Mengurangi Nilai Ke Variabel Dan Menetapkan Hasilnya) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data integer
+a -= 3   # --> Mengurangi nilai variabel a dan menetapkan hasilnya ke variabel itu sendiri
+print(a) # --> Mencetak variabel
+
+""" Penugasan (*=, Mengalikan Nilai Ke Variabel Dan Menetapkan Hasilnya) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data integer
+a *= 3   # --> Mengalikan nilai variabel a dan menetapkan hasilnya ke variabel itu sendiri
+print(a) # Mencetak variabel
+
+""" Penugasan (/=, Membagi Nilai Ke Variabel Dan Menetapkan Hasilnya)-(Float) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data integer
+a /= 3   # --> Mebagi nilai variabel a dan menetapkan hasilnya ke variabel itu sendiri (hasil pembagian float)
+print(a) # --> Mencetak variabel
+
+""" Penugasan (//=, Membagi Nilai Ke Variabel Dan Menetapkan Hasilnya)-(Integer) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data integer
+a //= 3   # --> Mebagi nilai variabel a dan menetapkan hasilnya ke variabel itu sendiri (hasil pembgian integer)
+print(a) # --> Mencetak variabel
+
+""" Penugasan (%=, Sisa Hasil Pembagian Nilai Ke Variabel Dan Menetapkan Hasilnya) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data integer
+a %= 3   # --> Sisa hasil pembagian nilai variabel a dan menetapkan hasilnya ke variabel itu sendiri
+print(a) # --> Mencetak variabel
+
+""" Penugasan (**=, Memangkatkan Nilai Ke Variabel Dan Menetapkan Hasilnya) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5    # --> Inisialisasi variabel yang menyimpan data inetegr
+a **= 3  # --> Memangkatkan nilai variabel a dan menetapkan hasilnya ke variabel itu sendiri
+print(a) # --> Mencetak variabel
+
+# 5. Operator Bitwise --> Untuk operasi bit per bit pada angka biner
+
+""" AND Bitwise (&) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi variabel yang menyimpan data integer (bit 0101)
+b = 3           # --> Inisialisasi variabel yang menyimpan data integer (bit 0011)
+results = a & b # --> Melakukan operasi AND bit per bit
+print(results)  # --> Mencetak variabel
+
+""" OR Bitwise (|) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi variabel yang menyimpan data integer (bit 0101)
+b = 3           # --> Inisialisasi variabel yang menyimpan data integer (bit 0011)
+results = a | b # --> Melakukan operasi OR bit per bit
+print(results)  # --> Mencetak variebl
+
+""" XOR Bitwise (^) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi variabel yang menyimpan data integer (bit 0101)
+b = 3           # --> Inisialisasi variabel yang menyimpan data integer (bit 0011)
+results = a ^ b # --> Melakukan operasi XOR bit per bit
+print(results)  # --> Mencetak variabel
+
+""" NOT Bitwise (~) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi variabel yang menyimpan data integer (bit 0101)
+results = ~ a   # --> Melakukan operasi NOT bit per bit (representasi biner dua komplemen)
+print(results)  # --> Mencetak variabel
+
+""" Shift Kiri Bitwise (<<) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5             # --> Inisialisasi variabel yang menyimpan data integer (bit 0101)
+results = a << 1  # --> Melakukan operasi Shift left atau menggeser bit ke kiri
+print(results)    # --> Mencetak variabel
+
+""" Shift Kanan Bitwise (>>) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5             # --> Inisialisasi variabel yang menyimpan data integer (bit 0101)
+results = a >> 1  # --> Melakukan operasi Shift right atau menggeser bit ke kanan
+print(results)    # --> Mencetak variabel
+
+# 6. Operator Keanggotaan --> Untuk memeriksa apakah suatu nilai ada dalam sebuah koleksi seperti list, tuple, set, string, dictionary (bisa juga di range)
+ 
+""" In (Mengembalikan True Jika Elemen Ada Dalam koleksi Dan Jika Elemen Tidak Ada Dalam Koleksi Maka Mengembalikan False ) """
+
+print("-------->")      # -- Abaikan ini
+
+my_list = [1, 2, 3, 4, 5]   # --> Inisialisasi variabel yang menyimpan data list, berisi 5 elemen data integer
+results = 2 in my_list      # --> Apakah elemen angka 2 berada dalam variabel my_list, jika ada maka mengembalikan True dan tersimpan pada variabel results
+print(results)              # --> Mencetak variabel
+
+my_tuple = (1, 2, 3, 4, 5)  # --> Inisialisasi variabel yang menyimpan data tuple, berisi 5 elemen data integer
+results = 2 in my_tuple     # --> Apakah elemen angka 2 berada dalam variabel my_tuple, jika ada maka mengembalikan True dan tersimpan pada variabel results
+print(results)              # --> Mencetak variabel
+
+my_set = {1, 2, 3, 4, 5}    # --> Inisialisasi variabel yang menyimpan data set, berisi 5 elemen data integer
+results = 2 in my_set       # --> Apakah angka 2 berada dalam variabel my_set, jika ada maka mengembalikan True dan tersimpan pada variabel results
+print(results)              # --> Mencetak hasil
+
+my_dictionary = {"nama":"Zaki", "Asal": "Rusia"} # --> Inisialisasi variabel yang menyimpan data dict, berisi 2 key-value data string
+results = "nama" in my_dictionary                # --> Apakah key nama dalam variabel my_dictionary, jika ada maka mengembalikan True dan tersimpan pada variabel results
+print(results)                                   # --> Mencetak variabel
+
+my_string = " Jangan Pernah Membuat Sakit Hati Seseorang Menggunakan kata " # --> Inisialisasi variabel yang menyimpan data string
+results = "Hati" in my_string                                               # --> Apakah hati berada dalam variabel my_string, jika ada maka mengembalikan True dan tersimpan pada variabel results
+print(results)                                                              # --> Mencetak variabel
+
+my_range = range(1, 5)  # --> Inisialisasi variabel yang menyimpan fungsi range atau urutan, berisi atau mencakup data integer 1 - 4
+results = 2 in my_range # --> Apakah, anka 2 berada dalam cakupan fungsi range di variabel my_range, jika ada maka mengembalikan True dan tersimpan pada variabel results
+print(results)          # --> Mencetak variabel
+
+
+""" Not In (Mengembalikan True Jika Elemen Tidak Ada Dalam Koleksi Dan Mengembalikan False Jika Elemen Berada Dalam Koleksi) """
+
+print("-------->")      # -- Abaikan ini
+
+my_list = [1, 2, 3, 4, 5]   # --> Inisialisasi variabel yang menyimpan data list, berisi 5 elemen data integer
+results = 6 not in my_list  # --> Apakah elemen angka 5 berada dalam variabel my_list, jika tidak ada maka mengembalikan True dan tersimpan pada variabel results
+print(results)              # --> Mencetak variabel
