38a734b8490f6404053d18360e25bf9783c22c51
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-07-22 11:09:07 +0700

    python-tiga

---
+++ Basic/operators_5.py
@@ -0,0 +1,170 @@
+""" All Operator """
+
+# 1. Operator Aritmatika --> Untuk melakukan operasi matematika
+
+""" Penjumlahan """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a + b  # --> Menjumlahkan nilai variabel a, b menggunakan operator + dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+""" Pengurangan """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a - b  # --> Mengurangi nilai variabel a, b menggunakan operatpr - dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+""" Perkalian """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi variabel yang menyimpan data integer
+b = 3           # --> Inisialisasi variabel yang menyimpan data integer
+results = 5 * 3 # --> Mengalikan nilai variabel a, b menggunakan operator * dan terimpan pada variabel results
+print(results)  # --> Mencetak variabel
+
+""" Pembagian (Float) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a / b  # --> Membagi milai variabel a, b menggunakan operator / (float) dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+""" Pembagian (Integer) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a // b # --> Membagi nilai variabel a, b menggunakan operator // (integer) dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+""" Modulo (Sisa Bagi) """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi variabel yang menyimpan data integer
+b = 3           # --> Inisialisasi variabel yang menyimpan data integer
+results = a % b # --> Sisa bagi nilai variabel a, b menggunakan operator % dan tersimpan pada variabel results
+print(results)  # --> Mencetak variabel
+
+""" Pemangkatan """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a ** b # --> Memangkatkan nilai variabel a, b menggunakan operator ** dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel 
+
+
+# 2. Operator Perbandingan --> Untuk membandingkan dua nilai  dan mengembalikan True atau False 
+
+""" Sama Dengan """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a == b # --> Menggunakan operator ==, apakah nilai variabel a sama dengan nilai variabel b dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+""" Tidak Sama Dengan """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a != b # --> Menggunakan operator !=, apakah nilai vaiabel a tidak sama dengan nilai variabel b dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+""" Lebih Besar Dari """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi vriabel yang menyimpan data integer
+b = 3           # --> Inisialisasi variabel yang menyimpan data integer
+results = a > b # --> Menggunakan operator >, apakah nilai variabel a lebih besar dari nilai variabel b dan tersimpan pada variabel results
+print(results)  # --> Mencetak variabel
+
+""" Lebih Kecil Dari """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5           # --> Inisialisasi variabel yang menyimpan data integer
+b = 3           # --> Inisialisasi variabel yang menyimpan data integer
+results = a < b # --> Menggunakan operator <, apakah nilai variabel a lebih kecil dari nilai variabel b dan tersimpan pada variabel results
+print(results)  # --> Mencetak variabel 
+
+""" Lebih Besar Atau Sama Dengan """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a >= b # --> Menggunakan operator >=, apakah nilai variabel a lebih besar sama dengan nilai variabel b dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+""" Lebih Kecil Atau Sama Dengan """
+
+print("-------->")      # -- Abaikan ini
+
+a = 5            # --> Inisialisasi variabel yang menyimpan data integer
+b = 3            # --> Inisialisasi variabel yang menyimpan data integer
+results = a <= b # --> Menggunakan operator <=, apakah nilai variabel a lebih kecil sama dengan nilai variabel b dan tersimpan pada variabel results
+print(results)   # --> Mencetak variabel
+
+
+# 3. Operator Logika --> Untuk menggabungkan kondisi-kondisi (Boolean)
+
+""" And (Mengembalikan True Jika Kedua Kondisi Bernilai True) """
+
+print("-------->")      # -- Abaikan ini
+
+a = True          # --> Inisialisasi variabel yang menyimpan data boolean
+b = False         # --> Inisialisasi variabel yang menyimpan data boolean
+results = a and b # --> Menggunakan operator and, menggabungkan variabel a, b lalu jika salah satu nilai vriabel False maka akan menghasilkan False dan tersimpan pada variabel results
+print(results)    # --> Mencetak variabel
+
+a = True          # --> Inisialisasi variabel yang menyimpan data boolean
+b = True          # --> Inisialisasi variabel yang menyimpan data boolean
+results = a and b # --> Menggunakan operator and, menggbaungkan variabel a, b lalu jika kedua nilai variabel True maka akan menghasilkan True dan terimpan pada variabel results
+print(results)    # --> Mencetak variabel
+
+a = False         # --> Inisialisasi variabel yang menyimpan data boolean
+b = False         # --> Inisialisasi variabel yang menyimpan data boolean
+results = a and b # --> Menggunakan operator and, menggbungkan variabel a, b lalu jika kedua nilai variabel False maka akan menghasilkan False dan tersimpan pada variabel results
+print(results)    # --> Mencetak variabel
+
+""" Or (Mengembalikan True Jika Salah Satu Kondisi Bernilai True) """
+
+print("-------->")      # -- Abaikan ini
+
+a = True          # --> Inisialisasi variabel yang menyimpan data boolean
+b = False         # --> Inisialisasi variabel yang menyimpan data boolean
+results = a or b  # --> Menggunakan operator or, menggabungkan nilai variabel a, b lalu jika salah satu nilai variabel True maka akan menghasilkan True dan tersimpan pada variabel results
+print(results)    # --> Mencetak variabel
+
+a = True          # --> Inisialisasi variabel yang menyimpan data boolean
+b = True          # --> Inisialisasi variabel yang menyimpan data boolean
+results = a or b  # --> Menggunakan operator or, menggabungkan nilai variabel a, b lalu jika kedua nilai variabel True maka akan menghasilkan True dan tersimpan pada variabel results
+print(results)    # --> Mencetak variabel
+
+a = False         # --> Inisialisasi variabel yang menyimpan data boolean
+b = False         # --> Inisialisasi variabel yang menyimpan data boolean
+results = a or b  # --> Menggunakan operator or, menggabungkan nilai variabel a, b lalu jika kedua nilai variabel False maka akan menghasilkan False dan tersimpan pada variabel results
+print(results)    # --> Mencetak variabel
+
+""" Not (Membalikan Nilai Kondisi) """
+
+print("-------->")      # -- Abaikan ini
+
+# ISTIRAHAT DUYU YAH
