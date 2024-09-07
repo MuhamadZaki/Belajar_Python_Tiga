793ad6b7b660461dbc5ed152727d18ebdc60d6ec
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-09-07 11:43:57 +0700

    python-tiga

--- Python-Tiga/Sesi12_Tipe_Data_Tuple.py
+++ Python-Tiga/Sesi12_Tipe_Data_Tuple.py
@@ -32,3 +32,43 @@ print("------")
 
 # 3. Perulangan tuple
 # Tuple Merupakan Salah Satu Tipe Data Yang Bisa Digunakan Secara langsung Pada Perulangan Menggunakan Keyword for
+
+a_tuple = (1,2,3,4,5)   # --> Inisialisasi variabel yang menyimpan data tuple, berisi 5 elemen data integer
+for e in a_tuple:       # --> Perulangan atau loop, mengiterasi melalui setiap element e dalam variabel a_list 
+    print(e)            # --> Mencetak variabel setiap iterasi, maka akan mencetak pesan 1,2,3,4,5
+
+
+# Perulangan Di Ekuvalen
+
+a_tuple = (1,2,3,4,5)            # --> Inisialisasi variabel yang menyimpan data tuple, berisi 5 elemen data integer
+for i in range(0, len(a_tuple)): # --> Perulangan atau loop, mengiterasi nilai i mulai rentang 0 hingga panjang a_tuple 
+    print(i, a_tuple[i])         # --> Mencetak variabel dan mencetak index juga elemen dari a_tuple, maka akan mencetak pesan  01 12 23 34 45
+
+
+# Fungsi enumerate()
+# Fungsi Ini Digunakan Untuk Membuat Data Sequence Menjadi Data enumrasi, Yang Jika Dimasukan Ke Perulangan Di Setiap Iterasinya Bisa Kisa Akses Index Berserta Elemennya
+
+a_tuple = (1,2,3,4,5)            # --> Inisialisasi variabel yang menyimpan data tuple, berisi 5 elemen data integer
+
+for i, n in enumerate(a_tuple):  # --> Perulangan atau loop, mengiterasi setiap index dan elemen dari a_tuple
+    print(i, n)                  # --> Mencetak variabel dan mencetak index juga elemen dari a_tuple, maka akan mencetak pesan 01 12 23 34 45
+
+
+print("------")
+
+#  4. Mencek Apakah Elemen Ada Pada tuple
+# Kombinasi keyword if Dan in Bisa Digunakan Untuk Mengidentifikasi Apakah Suatu Element Merupakan bagian Dari tuple Atau Tidak
+
+a_tuple = (1,2,3,4,5)  # --> Inisialisasi variabel yang menyimpan data tuple, berisi 5 elemen data integer
+e = 1                  # --> Inisialisasi variabel yang menyimpan data integer
+
+if e in a_tuple:       # --> Kondisi, memerikasa apakah nilai e, terdapat pada variabel a_tuple, jika kondisi terpenuhi maka akan mengeksekusi kode di dalamnya dan jika kondisi tidak terpenuhi maka akan melanjutkan ke kondisi berikutnya
+    print(True)        # --> Jika kondisi benar atau terpenuhi, maka akan mencetak pesan True
+else:                  # --> Kondisi, jika kondisi sebelumnya tidak terpenuhi maka blok kode ini dieksekusi
+    print(False)       # --> Jika kondisi salah atau tidak terpenuhi, maka akan mencetak pesan False
+
+
+print("------")
+
+# Nested tuple
+# Dibuat Dengan Menuliskan Data tuple Sebagai ELemen tuple
