1a5d7e63ecb1c0f33341cb89fabc52f99d50e7f7
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-09-30 15:43:31 +0700

    python-tiga

---
+++ Python-Tiga/Sesi14_Tipe_Data_Dictionary.py
@@ -0,0 +1,12 @@
+# Salah Satu Tipe Data mapping di Python, Yaitu dictionary
+
+# 1. Pengenalan dictionary
+# Dictionary atau dict Merupakan Tipe Data Kolektif berbentuk key-value
+
+bio = {
+    "id":2,
+    "name":"Laura",
+    "perempuan":True
+}
+
+# Literal dict Tertulis Dengan Menggunakan {}, Mirip Seperti Tipe Data Set, Hanya Saja bedanya Pada Tipe dictionary Isinya Berbentuk keu-value
