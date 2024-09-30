334f03dd75b966b00e999fcd912b54cf2739f3d4
MuhamadZaki <Muhamadzaki992@gmail.com>
2024-09-30 15:36:46 +0700

    python-tiga

--- Python-Tiga/Sesi13_Tipe_data_Set.py
+++ Python-Tiga/Sesi13_Tipe_data_Set.py
@@ -175,4 +175,129 @@ print(results)                      # --> Mencetak variabel, maka akan mencetak 
 
 
 # Pengecekan Keanggotaan Atau Membership disjoint
-# Method isdisjoint() Mengembalikan Nilai True Jika set Pada Pemanggilan Fungsi Berisi Elemen Yang Semuanya Bukan Anggota Data Method Dipanggil
+# Method isdisjoint() Mengembalikan Nilai True Jika set Pada Pemanggilan Fungsi Berisi Elemen Yang Semuanya Bukan Anggota Data Method Dipanggil Atau Digunakan Untuk Memeriksa Apakah Dua set Tidak Memiliki Elemen Yang Sama
+
+datas = {"satu", "dua", "tiga"}             # --> Inisialisasi variabel yang menyimpan data set, berisi 3 elemen data string
+a_set = datas.isdisjoint({"satu", "dua"})   # --> Memeriksa apakah variabel datas tidak memiliki elemen yang sama dengan variabel a_set dan dalam kasus ini terdapat 2 elemen yang match
+print(a_set)                                # --> Mencetak vriabel, maka akan mencetak pesan False
+
+datas = {"satu", "dua", "tiga"}             # --> Inisialisasi variabel yang menyimpan data set, berisi 3 elemen data string
+b_set = datas.isdisjoint({"tiga", "empat"}) # --> Memeriksa apakah variabel datas tidak memiliki elemen yang sama dengan variabel b_set dan dalam kasus ini terdapat 1 elemen yang match
+print(b_set)                                # --> Mencetak variabel, maka akan mencetak pesan false
+
+datas = {"satu", "dua", "tiga"}             # --> Inisialisasi variabel yang menyimpan data set, berisi 3 elemen data string
+c_set = datas.isdisjoint({"lima"})          # --> Memeriksa apakah variabel datas tidak memiliki elemen yang sama dengan variabel b_set dan dalam kasus ini tidak ada elemen yang match
+print(c_set)                                # --> Mencetak variabel, naka akan mencetak pesan True
+
+
+# Extend / Concat Atau Union Element
+# Opersi extend Merupakan Operasi Penggabungan Duat Data set Dan Ada beberapa Metode Yang Tersedia
+
+# Method union()
+jaki = {"satu", "dua", "tiga"}                           # --> Inisialisasi variabel yang menyinpan data set, berisi 3 elemen data string
+laura = {"empat", "satu"}                                # --> Inisialisasi variabel yang menyimpan data set, berisi 2 elemen data string
+natasia = {"lima"}                                       # --> Inisialisasi variabel yang menyimpan data set, berisi 1 elemen data string
+lolimal = {"enam", "tujuh"}                              # --> inisialisasi variabel yang menyimpan data set, berisi 2 elemen data string
+
+datas = jaki.union(laura).union(natasia).union(lolimal)  # --> Menggabungkan semua elemen dari beberapa variabel, duplikasi dihilangkan dan akan menghasilkan elemen unik
+print(datas)                                             # --> Mencetak variabel, maka akan mencetak pesan {'empat', 'tiga', 'satu', 'enam', 'tujuh', 'dua', 'lima'}
+
+# Mehtod update()
+jaki = {"satu", "dua", "tiga"}                           # --> Inisialisasi variabel yang menyinpan data set, berisi 3 elemen data string
+laura = {"empat", "satu"}                                # --> Inisialisasi variabel yang menyimpan data set, berisi 2 elemen data string
+natasia = {"lima"}                                       # --> Inisialisasi variabel yang menyimpan data set, berisi 1 elemen data string
+lolimal = {"enam", "tujuh"}                              # --> inisialisasi variabel yang menyimpan data set, berisi 2 elemen data string
+
+datas = set()                                            # --> Inisialisasi vriabel yang menyimpan data set, berisi elemen kosong
+datas.update(jaki)                                       # --> Mengubah nilai variabel datas dengan memanggil nilai atau elemen variabel jaki
+datas.update(laura, natasia, lolimal)                    # --> Menggabungkan semua elemen dari beberapa variabel, duplikasi dihilangkan dan akan menghasilkan elemen unik
+print(datas)                                             # --> Mencetak variabel, maka akan mencetak pesan {'tujuh', 'tiga', 'enam', 'empat', 'dua', 'lima', 'satu'}
+
+
+# Operator bitwise Pada set
+
+# Opersi or Pada set Menggunakan Operator |
+a = set("abcdefghi") # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+b = set("almnopq")   # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+results = a | b      # --> Menggabungkan elemen dari kedua variabel dan tersimpan pada variabel results
+print(results)       # --> Mencetak variabel, maka akan mencetak pesan {'l', 'h', 'f', 'g', 'i', 'n', 'o', 'p', 'c', 'q', 'm', 'd', 'e', 'a', 'b'}
+
+# Operasi and Pada set Menggunakan Operator &
+a = set("abcdefghi") # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+b = set("almnopq")   # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+results = a & b      # --> Memeriksa irisan (intersection) antara variabel a, b dan jika ada elemen yang sama dikedua set maka itulah yang akan muncul dan tersimpan pada variabel results
+print(results)       # --> Mencetak variabel, maka akan mencetak pesan {'a'}
+
+# Operasi Exclusive or Pada set Menggunakan Operator ^
+a = set("abcdefghi") # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+b = set("almnopq")   # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+results = a ^ b      # --> Jika ada kesamaan elemen pada variabel a dan b maka tidak akan ikut tampil atau nilai results berisi elemen set yang ada di variabel a atau b tetapi tidak ada dikeduanya
+print(results)       # --> Mencetak vriabel, maka akan mencetak pesan {'b', 'n', 'f', 'o', 'c', 'i', 'h', 'm', 'p', 'l', 'g', 'e', 'q', 'd'}
+
+
+# Operator - Pada set
+# Digunakan Untuk Pencarian Perbedaan Elemen
+
+a = set("abcdefghi") # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+b = set("almnopq")   # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string
+results = a-b        # --> Nilai results berisi elemen set unik yang merupakan anggota set a tapi bukan anggota set b atau jika ada kesamaan elemen dari variabel a dan b maka duplikasi dihilangkan
+print(results)       # --> Mencetak variabel, naka akan mencetak pesan {'i', 'g', 'b', 'e', 'c', 'f', 'h', 'd'}
+
+
+
+print("------")
+
+# 4. Fungsi set()
+
+# Konversi string Ke set
+# String Dibungkus Menggunakan Method set() Dan Menghasilkan Data set Berisi karakter string Yang Unik
+
+datas = set("abcde") # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data string (mengkonversi)
+print(datas)         # --> Mencetak vriabel, maka akan mencetak pesan {'e', 'c', 'b', 'd', 'a'}
+
+
+# Konversi list Ke set
+# Data list Bisa Diubah Menjadi set Dengan Mudah Dan Cara Membungkusnya Menggunakan Fungsi set()
+
+datas = set(["a", "b", "c", "d"]) # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data list (mengkonversi)
+print(datas)                      # --> Mencetak variabel, maka akan mencetak pesan {'b', 'a', 'd', 'c'}
+
+
+# Konversi tuple Ke set
+# Data tuple Juga Bisa Diubah Menjadi set Via fungsi set()
+
+datas = set(("a", "b", "c", "d")) # --> Inisialisasi variabel yang menyimpan data set, berisi elemen data tuple (mengkonversi)
+print(datas)                      # --> Mencetak variabel, maka akan mencetak pesan {'b', 'd', 'c', 'a'}
+
+
+# Konversi range Ke set
+# Data range (Hasil dari Pemanggilan fungsi range()) Bisa Dikonversi Ke Bentuk set Via fungsi set()
+
+datas = set(range(1,4))           # --> Inisialisasi variabel yang menyimpan data set, berisi range (mengkonversinya)
+print(datas)                      # --> Mencetak variabel, maka akan mencetak pesan {1, 2, 3}
+
+
+
+print("------")
+
+# 5. set comprehension
+# Method comprehension Juga Bisa Diterapkan Pada set
+
+results = {a for a in set("abcdef") if a not in set("agh")} # --> Membuat set results dengan elemen-elemen yang ada di "abcdef" tetapi tidak ada di "agh"
+print(results)                                              # --> Mencetak variabel, maka akan mencetak pesan {'f', 'd', 'c', 'b', 'e'}
+
+
+
+print("------")
+
+# 6. frozenset
+# frozenset Merupakan set Yang immutable Atau Tidak Bisa Diubah Nilai elemennya Setelah Didekalrasikan
+
+a = frozenset("abcdef") # --> Inisialisasi variabel yang menyimpan data frozenset, berisi elemen data setring
+print(a)                # --> Mencetak variabel, maka akan mencetak pesan frozenset({'b', 'e', 'd', 'f', 'a', 'c'})
+print(type(a))          # --> Mencetak variabel dan menegcek tipe data, maka akan mencetak pesan <class 'frozenset'>
+
+n = frozenset("aiqo")   # --> Inisialisasi variabel yang menyimpan data frozenset, berisi elemen string
+print(n)                # --> mencetak variabel, mak akan mencetak pesan frozenset({'q', 'a', 'o', 'i'})
+
+# Semua Operasi set, Method Milik set Bisa Digunakan Pada frozenset, Kecuali Beberapa Operasi Yang Sifatnya Mutable Atau Mengubah Elemen. Contohnya Seperti Method add(), pop(), remove() Dan Lainnya Tidak Bisa Digunakan Di frozenset.
